c7cf37dfd7577145b276a78e2a9f21a802772ea5
Jonathan A. Leaver <jleaver@alumni.uwo.ca>
2025-11-07 21:30:58 -0500

    activate camera and capture 10 frames

--- Ayla.Tests/CaptureTest.fs
+++ Ayla.Tests/CaptureTest.fs
@@ -1,10 +1,48 @@
 ﻿namespace Ayla.Tests
 
+open Ayla
+open FlashCap
 open NUnit.Framework
 
 [<TestFixture>]
 type CaptureTest() =
-
+    
     [<Test>]
-    member x.TestCapture1() =
-        Assert.AreEqual(1,1)
+    member x.TestDevicesQuery() =
+        let devices = Capture.Devices.list()
+        Assert.IsNotEmpty(devices)
+        
+    [<Test>]
+    member x.TestFormatsQuery() =
+        let devices = Capture.Devices.list()
+        let formatsByDevice =
+            devices
+            |> Seq.map(fun device -> device.Name, Capture.Devices.formats device)
+            |> Map.ofSeq
+        let formats =
+            formatsByDevice
+            |> Map.toSeq
+            |> Seq.collect (fun (name, formats) ->
+                formats
+                |> Seq.map (fun format -> format.Description))
+            |> Set.ofSeq
+        Assert.IsNotEmpty(formats)
+        
+        
+    //YUYV, NV12 (640x360) for Dell, (1920x1080) for Logitech
+    [<Test>]
+    member x.TestConsumeStream() =
+        let devices = Capture.Devices.list()
+        let logitech = devices |> Seq.find (fun device -> device.Description = "Logitech BRIO (DirectShow)")
+        let formats = Capture.Devices.formats logitech
+        let yuyv30fps =
+            formats
+            |> Seq.filter (fun format -> format.PixelFormat = PixelFormats.YUYV)
+            |> Seq.filter (fun format -> format.FramesPerSecond.Numerator = 30 &&
+                                         format.FramesPerSecond.Denominator = 1)
+            |> Seq.maxBy (fun format -> format.Height)
+        let frames =
+            Capture.stream(logitech, yuyv30fps)
+            |> Seq.take 10
+            |> Seq.toList
+        ()
--- Ayla/Capture.fs
+++ Ayla/Capture.fs
@@ -1,6 +1,42 @@
 ﻿namespace Ayla
 
+open System
+open System.Collections.Concurrent
+open System.Threading
+open FlashCap
+
 module Capture =
     
-    let hello name =
-        printfn "Hello %s" name
+    module Devices =
+        
+        let list() =
+            let devices = new CaptureDevices()
+            devices.GetDescriptors()
+            |> List.ofArray
+            
+        let formats(descriptor:CaptureDeviceDescriptor) =
+            descriptor.Characteristics
+            |> List.ofArray
+        
+    let stream(device:CaptureDeviceDescriptor, format:VideoCharacteristics) =
+        seq {
+            use queue = new BlockingCollection<_>()
+            let enqueue (arrived:PixelBufferScope) =
+                let buffer : PixelBuffer = arrived.Buffer
+                let event =
+                    let index = buffer.FrameIndex
+                    let timestamp = buffer.Timestamp
+                    let bytes = buffer.CopyImage()
+                    index, timestamp, bytes
+                arrived.ReleaseNow()
+                queue.Add(event)
+            let arrived = new PixelBufferArrivedDelegate(enqueue)
+            use source = new CancellationTokenSource()
+            use subscription = device.OpenAsync(format,arrived,source.Token).Result
+
+            subscription.StartAsync().Wait()
+            use unsubscribe =
+                { new IDisposable with member x.Dispose() = subscription.StopAsync().Wait() }
+            while true do
+                yield queue.Take(source.Token)
+        }
